ed106c28e44cb7b0cebb4096f5e44ea88525e117
rodio <rodionmarkelov01229@gmail.com>
2023-12-06 15:04:28 +0400

    Добавьте файлы проекта.

---
+++ sort_lab/sort_lab.cpp
@@ -0,0 +1,38 @@
+﻿#include "sort_lab.h"
+#include <vector>
+#include <random>
+
+using namespace std;
+
+void print(std::vector<int> list) {
+	cout << "Vector:" << endl;
+	for (int i = 0; i < list.size(); ++i) {
+		cout << list[i] << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	std::vector<int> list;
+	srand(0);
+	for (int i = 0; i < 10; ++i) {
+		list.push_back(rand());
+	}
+	print(list);
+	insertionSort(list);
+	print(list);
+
+	std::vector<int> list1;
+	list1.push_back(10);
+	srand(0);
+
+	for (int i = 0; i < 10; ++i) {
+		list1.push_back(10-i);
+	}
+	print(list1);
+	quickSort(list1, 0, list.size() - 1);
+	print(list1);
+
+	return 0;
+}
---
+++ sort_lab/sort_lab.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+struct stats {
+	size_t comparison_count = 0;
+	size_t copy_count = 0;
+};
+
+void insertionSort(std::vector<int>& list) {
+	for (int i = 1; i < list.size(); ++i) {
+		int key = list[i];
+		int j = i - 1;
+		while (j >= 0 && list[j] > key) {
+			list[j+1] = list[j];
+			j = j - 1;
+		}
+		list[j + 1] = key;
+	}
+}
+
+void quickSort(std::vector<int>& list, int start, int end) {
+	if (start >= end) return;
+	int pivot = list[end];
+	int pIndex = start;
+	for (int i = start; i < end; i++)
+	{
+		if (list[i] <= pivot)
+		{
+			swap(list[i], list[pIndex]);
+			pIndex++;
+		}
+	}
+	swap(list[pIndex], list[end]);
+	quickSort(list, start, pIndex - 1);
+	quickSort(list, pIndex + 1, end);
+}
